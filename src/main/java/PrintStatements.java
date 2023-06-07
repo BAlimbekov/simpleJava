f922a50cdebd9cbeb472a53895965d24f96da0f3
BAlimbekov <alimbekovbekmambet@gmail.com>
2023-06-07 10:06:50 -0500

    first commit

---
+++ src/main/java/PrintStatements.java
@@ -0,0 +1,39 @@
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
+public class PrintStatements {
+
+    public static void main (String [] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Bekmambet.");
+        System.out.println("This is my first Java app!");
+        System.out.println("So, congratulate me!");
+        System.out.println("I am pumped about this course.");
+        System.out.println("Let's do some experiment.");
+        System.out.println("I will add to integers in the next line.");
+        System.out.println("Here we go! 2+2");
+        System.out.println("It did not add to digits.");
+        System.out.println("I guess it because the type of date is string.");
+        System.out.println("Next I will try to add to integers without quotes.");
+        System.out.println(2+2);
+        System.out.println("It wooooorked!!!!");
+        System.out.println("So, congratulate me again!");
+        System.out.println("Next, I will try to add two different types of data.");
+        System.out.println(2 + "WORLD");
+        System.out.println("Interesting, I was able to add number with string.");
+        System.out.println("Let's think about different experiments.");
+        System.out.println("How about if I multiply string with number?");
+        System.out.println(2 + "Hello");
+        System.out.println("So, I did try to subtract, multiply and divide number by string.");
+        System.out.println("It was unsuccessfull.");
+        System.out.println("Conclusion is that you can add number with string using + sign.");
+        System.out.println("However, you can't multiply, divide or subtract");
+        System.out.println("This is good to know!");
+        System.out.println("How about multiply a string with a number?");
+        System.out.println("Hello * 2");
+        System.out.println("The above operation failed");
+        System.out.println("Two more statements and I will be done with my assignment");
+        System.out.println("I should be my last statement!");
+        System.out.println("Take care! Bye!");
+
+    }
+}
